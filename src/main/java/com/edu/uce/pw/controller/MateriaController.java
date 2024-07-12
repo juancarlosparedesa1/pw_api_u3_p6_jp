7a0c03a895ba2226e5003731a30f0aae1207667b
juancarlosparedesa1 <jcparedesa1@uce.edu.ec>
2024-07-12 16:37:45 -0500

    tare07

--- src/main/java/com/edu/uce/pw/controller/MateriaController.java
+++ src/main/java/com/edu/uce/pw/controller/MateriaController.java
@@ -3,6 +3,7 @@ package com.edu.uce.pw.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,38 +30,55 @@ public class MateriaController {
 	// http://localhost:8080/API/v1.0/Matricula/materias/guardar
 	@PostMapping
 	//Nivel1:  http://localhost:8080/API/v1.0/Matricula/materias
-	public  ResponseEntity<Materia> guardar(@RequestBody Materia mate) {
-		this.materiaService.ingresar(mate);
-		return ResponseEntity.status(201).body(mate);
-	}
+	  public ResponseEntity<Materia> guardar(@RequestBody Materia materia) {
+	  	HttpHeaders headers= new HttpHeaders();
+	  	headers.add("mensaje_200", "Materia insertada");
+        materiaService.ingresar(materia);
+       
+       // return ResponseEntity.status(200).body(materia);
+        return new ResponseEntity<Materia>(materia, headers,200);
+    }
 
 	// http://localhost:8080/API/v1.0/Matricula/materias/actualizar/
-	// Nivel1:http://localhost:8080/API/v1.0/Matricula/materias/actualizar/1
+	// Nivel1:http://localhost:8080/API/v1.0/Matricula/materias/1
 
-	@PutMapping(path="/{id}")
-	public ResponseEntity<Materia> actualizar(@RequestBody Materia mate,@PathVariable Integer id) {
-		this.materiaService.actualizar(mate);
-		return ResponseEntity.status(238).body(mate);
-	}
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Materia> actualizar(@PathVariable Integer id, @RequestBody Materia materia) {
+    	HttpHeaders headers= new HttpHeaders();
+    	headers.add("mensaje_238","Materia actualizada");
+        Materia mater = materiaService.buscar(id);
+        if (mater != null) {
+            materia.setId(id);
+            materiaService.actualizar(materia);
+        }
+       // return ResponseEntity.status(238).body(materia);
+        
+        return new ResponseEntity<Materia>(materia, headers, 238);
+    }
 
 	// http://localhost:8080/API/v1.0/Matricula/materias/actualizarParcial
 	//Nivel 1: http://localhost:8080/API/v1.0/Matricula/materias/4
 
-	@PatchMapping(path="/{id}")
-	public ResponseEntity<Materia> actualizarParcial(@RequestBody Materia mate,@PathVariable Integer id) {
-		Materia mate2 = this.materiaService.buscar(mate.getId());
-		if (mate.getNombre() != null) {
-			mate2.setNombre(mate.getNombre());
-		}
-		if (mate.getCodigo() != null) {
-			mate2.setCodigo(mate.getCodigo());
-		}
-		if (mate.getNumeroCreditos() != null) {
-			mate2.setNumeroCreditos(mate.getNumeroCreditos());
-		}
-		this.materiaService.actualizar(mate2);
-		return ResponseEntity.status(239).body(mate2);
-	}
+    @PatchMapping(path = "/{id}")
+    public ResponseEntity<Materia> actualizarParcial(@PathVariable Integer id, @RequestBody Materia materia) {
+        Materia mater = materiaService.buscar(id);
+        HttpHeaders headers= new HttpHeaders();
+        headers.add("mensaje_239", "Materia actuailzada pacialmente");
+        
+        if (mater != null) {
+            if (materia.getNombre() != null) {
+                mater.setNombre(materia.getNombre());
+            }
+            
+            if (materia.getNumeroCreditos() != null) {
+                mater.setNumeroCreditos(materia.getNumeroCreditos());
+            }            
+            materiaService.actualizar(mater);
+        }
+        
+        //return ResponseEntity.status(239).body(materia);
+        return new ResponseEntity<Materia> (materia, headers, 239);
+    }
 
 	// http://localhost:8080/API/v1.0/Matricula/materias/borrar
 	// http://localhost:8080/API/v1.0/Matricula/materias/borrar/1
@@ -71,19 +89,28 @@ public class MateriaController {
 	//Nivel 1: http://localhost:8080/API/v1.0/Matricula/materias/3
 
 
-	@DeleteMapping(path = "/{id}")
-	public ResponseEntity<String> borrar(@PathVariable Integer id) {
-		this.materiaService.borrar(id);
-		return ResponseEntity.status(240).body("Borrado");
-	}
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<String> borrar(@PathVariable Integer id) {
+        materiaService.borrar(id);
+        HttpHeaders headers= new  HttpHeaders();
+        headers.add("mensaje_240", "Materia borrada");
+        //return ResponseEntity.status(240).body("Borrado");
+        
+        return new ResponseEntity<String>("Borrado", headers,240);
+    }
 
 	
 	// http://localhost:8080/API/v1.0/Matricula/materias/1
 
-	@GetMapping(path = "/{id}")
-	public ResponseEntity<Materia> buscar(@PathVariable Integer id) {
-		return ResponseEntity.status(240).body(this.materiaService.buscar(id));
-	}
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Materia> buscar(@PathVariable Integer id) {
+    	Materia m= materiaService.buscar(id);
+    	
+    	HttpHeaders headers= new HttpHeaders();
+    	// return ResponseEntity.status(236).body(m);
+    	
+    	return new ResponseEntity<Materia>(m, headers, 236);
+    }
 	
 	// Nivel 1: http://localhost:8080/API/v1.0/Matricula/materias/buscar/creditos
 
